16a20f08cc517bd64e31396f1953a7cbc7eef4f3
james ban <jban0811@gmail.com>
2023-03-31 21:24:49 +1300

    portfolio page changes

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,13 +1,22 @@
-import { Box, Paper, Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 
 import Cover from '../../resources/OsstemDentalEshop/Cover.jpg';
+import Cover2 from '../../resources/OsstemDentalEshop/Cover2.jpg';
 
 export function Portfolio() {
     return (
         <Stack spacing={1}>
-            <img alt="Cover" src={Cover} />
+            <img
+                alt="Cover"
+                src={Cover2}
+                // style={{ maxHeight: '70vh', objectFit: 'cover' }}
+            />
             <Box>
-                <Typography>Commercial</Typography>
+                <Box sx={{ display: 'flex', flexDirection: 'row', marginTop: 1 }}>
+                    <Typography sx={{ marginRight: 3 }}>Commercial</Typography>
+                    <Typography sx={{ marginRight: 3 }}>Application</Typography>
+                    <Typography sx={{ marginRight: 3 }}>Feb 2022 - May 2022</Typography>
+                </Box>
                 <Typography variant="h1">Osstem dental e-shop</Typography>
             </Box>
         </Stack>
