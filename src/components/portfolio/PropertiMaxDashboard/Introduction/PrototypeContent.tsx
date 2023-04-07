ce0d658c8edadeb336267f92134bc366f98b7370
james ban <jban0811@gmail.com>
2023-04-07 23:37:57 +1200

    prototypelinks

--- src/components/portfolio/PropertiMaxDashboard/Introduction/PrototypeContent.tsx
+++ src/components/portfolio/PropertiMaxDashboard/Introduction/PrototypeContent.tsx
@@ -3,7 +3,7 @@ import { PortfolioPrototype, PortfolioSectionContainer } from '@/components';
 export const PrototypeContent = () => {
     return (
         <PortfolioSectionContainer>
-            <PortfolioPrototype src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FYaSg3yZwKE74FdL2zDd1j7%2FPropertiMax-App%3Fnode-id%3D1-19%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D1%253A19%26show-proto-sidebar%3D1" />
+            <PortfolioPrototype src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F6K5JJRnSE3GxvVMopCSLK0%2FUntitled%3Fnode-id%3D4-20013%26scaling%3Dcontain%26page-id%3D0%253A1%26starting-point-node-id%3D4%253A15328" />
         </PortfolioSectionContainer>
     );
 };
