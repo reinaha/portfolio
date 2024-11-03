6d42be5e333e4b6ca3824f9f5bd12565e290ab60
james ban <jban0811@gmail.com>
2024-11-03 23:39:28 +1300

    Update new nz post items on home

--- src/data/PortfolioCardData.tsx
+++ src/data/PortfolioCardData.tsx
@@ -2,10 +2,14 @@ import { NavigateFunction } from 'react-router-dom';
 
 import { FernIcon } from '@/icons';
 import {
+    AnzacStampBackground,
     ChristmasStampBackground,
     DentalECommerceApp,
     DentalECommerceWeb,
     GraphicDesignWork,
+    NZPostBrandDesign,
+    NZPostDigitalDesign,
+    NZPostMotionDesign,
     PostcrossingBackground,
     RealEstateTradingApp,
     RealEstateTradingDashboard,
@@ -73,6 +77,60 @@ export const PortfolioCardData: IPortfolioCard[] = [
             ),
     },
     {
+        title: '2025 \nANZAC \nStamps Design.',
+        subtitle: '／ New Zealand Official Stamp',
+        //tags: ['UI/UX Design', 'Mobile App'],
+        backgroundImg: AnzacStampBackground,
+        textColor: '#5C3C9C',
+        titleProps: {
+            fontSize: '42px',
+            fontWeight: 700,
+            fontFamily: 'Inter',
+            lineHeight: '40px',
+            letterSpacing: '-3%',
+            whiteSpace: 'pre-wrap',
+        },
+        subtitleProps: {
+            fontSize: '18px',
+            fontWeight: 700,
+            fontFamily: 'Inter',
+            lineHeight: '20px',
+            letterSpacing: '-3%',
+        },
+        titleSubtitleSpacing: 7,
+        titleIcon: <FernIcon color="#5C3C9C" />,
+        onClick: () => () =>
+            window.open(
+                'https://www.figma.com/proto/1ShL40xopOSV4m14xDQ1VD/2025-ANZAC-Stamp?node-id=6001-1043&node-type=frame&t=URbt28XfPGuNbsWJ-1&scaling=min-zoom&content-scaling=fixed&page-id=4003%3A1517',
+                '_blank'
+            ),
+    },
+    {
+        title: 'NZ Post Brand Design',
+        subtitle: 'Implementing Brand Guideline',
+        //tags: ['UI/UX Design', 'Mobile App'],
+        backgroundImg: NZPostBrandDesign,
+        onClick: () => () =>
+            window.open(
+                'https://www.figma.com/proto/7zTryw9FH0BwNXd76iiq9i/NZPost-Brand-Design?node-id=6001-1043&node-type=frame&t=7cMmig6Gv0pM8idh-1&scaling=min-zoom&content-scaling=fixed&page-id=4003%3A1517',
+                '_blank'
+            ),
+    },
+    {
+        title: 'NZ Post Digital Design',
+        subtitle: 'Digital Advertisement',
+        //tags: ['UI/UX Design', 'Mobile App'],
+        backgroundImg: NZPostDigitalDesign,
+        onClick: () => () => console.debug('Not implemented'),
+    },
+    {
+        title: 'NZ Post Motion Design',
+        subtitle: 'Video and Motion',
+        //tags: ['UI/UX Design', 'Mobile App'],
+        backgroundImg: NZPostMotionDesign,
+        onClick: () => () => console.debug('Not implemented'),
+    },
+    {
         title: 'PropertiMax Mobile App',
         subtitle: 'Real Estate Trading Application',
         //tags: ['UI/UX Design', 'Mobile App'],
@@ -113,6 +171,10 @@ export const PortfolioCardData: IPortfolioCard[] = [
         title: 'Graphic Design Works',
         //tags: ['Graphic Design', 'Application'],
         backgroundImg: GraphicDesignWork,
-        onClick: () => () => window.open('https://bit.ly/suminGraphics', '_blank'),
+        onClick: () => () =>
+            window.open(
+                'https://www.figma.com/proto/TrZnUn80G7cYcBDdfirX8Z/%F0%9F%91%88-To-view-mores-check-the-side-bar-%F0%9F%91%88-2024-Design?node-id=6089-13412&node-type=canvas&t=biRVzw0sj7G5WLW4-1&scaling=min-zoom&content-scaling=fixed&page-id=6089%3A48&starting-point-node-id=6089%3A13412&show-proto-sidebar=1',
+                '_blank'
+            ),
     },
 ];
--- src/images/HomeImages.ts
+++ src/images/HomeImages.ts
@@ -1,17 +1,25 @@
+import AnzacStampBackground from '@resources/Home/AnzacStampBackground.png';
 import ChristmasStampBackground from '@resources/Home/ChristmasStampBackground.png';
 import DentalECommerceApp from '@resources/Home/DentalECommerceApp.png';
 import DentalECommerceWeb from '@resources/Home/DentalECommerceWeb.png';
 import GraphicDesignWork from '@resources/Home/GraphicDesignWork.png';
+import NZPostBrandDesign from '@resources/Home/NZPostBrandDesign.png';
+import NZPostDigitalDesign from '@resources/Home/NZPostDigitalDesign.png';
+import NZPostMotionDesign from '@resources/Home/NZPostMotionDesign.png';
 import PostcrossingBackground from '@resources/Home/PostcrossingBackground.png';
 import RealEstateTradingApp from '@resources/Home/RealEstateTradingApp.png';
 import RealEstateTradingDashboard from '@resources/Home/RealEstateTradingDashboard.png';
 import RealEstateTradingWeb from '@resources/Home/RealEstateTradingWeb.png';
 
 export {
+    AnzacStampBackground,
     ChristmasStampBackground,
     DentalECommerceApp,
     DentalECommerceWeb,
     GraphicDesignWork,
+    NZPostBrandDesign,
+    NZPostDigitalDesign,
+    NZPostMotionDesign,
     PostcrossingBackground,
     RealEstateTradingApp,
     RealEstateTradingDashboard,
